d4177509f032f47ddf17824e1348900779af4abc
Michael Ray <michaelray2809@yahoo.com>
2024-09-16 11:45:22 +0000

    Added overflow-x: hidden

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -2,7 +2,10 @@ var navLinks = document.getElementById("navLinks");
 
         function showMenu() {
             navLinks.style.right = "-60px";
+            
         }
         function hideMenu() {
             navLinks.style.right = "-210px";
+
         }
+
